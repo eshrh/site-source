2e1c505117249b8f1ce666c9ff2ccca55891f994
eshrh <esrh@gatech.edu>
2022-06-20 03:19:45 -0400

    website update

--- site.hs
+++ site.hs
@@ -1,47 +1,51 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Monoid (mappend)
-import Data.List (isPrefixOf)
-import Data.Text (Text)
-import Data.Functor.Identity (runIdentity)
-import qualified Data.Text as T
+
 import Control.Monad (mapM_)
+import Data.Functor
+import Data.Functor.Identity (runIdentity)
+import Data.List (isPrefixOf)
+import Data.Monoid (mappend)
+import Data.Text (Text)
+import qualified Data.Text as T
 import Hakyll
 import Hakyll.Web.Html (demoteHeaders)
-import System.Directory (copyFile,
-                         getHomeDirectory)
-import System.FilePath (FilePath, joinPath)
-import Skylighting (zenburn, styleToCss)
+import Skylighting (styleToCss, zenburn)
 import Skylighting.Styles
-import Text.Pandoc.Options      (ReaderOptions (..), WriterOptions (..))
-import qualified Text.Pandoc.Templates (Template)
-import Text.Pandoc.Templates (compileTemplate)
+import System.Directory
+  ( copyFile,
+    getHomeDirectory,
+  )
+import System.FilePath (FilePath, joinPath)
 import System.Posix.Internals (newFilePath)
+import Text.Pandoc.Options (ReaderOptions (..), WriterOptions (..))
+import Text.Pandoc.Templates (compileTemplate)
+import qualified Text.Pandoc.Templates (Template)
+
 --------------------------------------------------------------------------------
 pandocCodeStyle = zenburn
 
-
 tocTemplate :: Text.Pandoc.Templates.Template Text
-tocTemplate = either error id . runIdentity . compileTemplate "" $ T.unlines
-  [ "<h2 class=\"tocheader\">Contents</h2>"
-  , "<div class=\"toc\">"
-  , "$toc$"
-  , "</div>"
-  , "$body$"
-  ]
-
+tocTemplate =
+  either error id . runIdentity . compileTemplate "" $
+    T.unlines
+      [ "<h2 class=\"tocheader\">Contents</h2>",
+        "<div class=\"toc\">",
+        "$toc$",
+        "</div>",
+        "$body$"
+      ]
 
 pandocCompilerWithOpts :: Compiler (Item String)
 pandocCompilerWithOpts =
   pandocCompilerWith
     defaultHakyllReaderOptions
     defaultHakyllWriterOptions
-      { writerTableOfContents  = True,
-        writerNumberSections   = True,
-        writerTOCDepth         = 2,
-        writerTemplate         = Just tocTemplate}
-
-
+      { writerTableOfContents = True,
+        writerNumberSections = True,
+        writerTOCDepth = 2,
+        writerTemplate = Just tocTemplate
+      }
 
 expandHome :: FilePath -> String -> FilePath
 expandHome home s
@@ -59,61 +63,61 @@ main = do
   mapM_ (syncOne homedir) syncFiles
 
   writeFile "_site/css/syntax.css" $ styleToCss zenburn
-  
+
   hakyll $ do
     match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+      route idRoute
+      compile copyFileCompiler
 
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+      route idRoute
+      compile compressCssCompiler
 
     match "files/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+      route idRoute
+      compile copyFileCompiler
 
     match "pages/*" $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+      route $ setExtension "html"
+      compile $
+        pandocCompiler
+          >>= loadAndApplyTemplate "templates/default.html" defaultContext
+          >>= relativizeUrls
 
     match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompilerWithOpts
-            >>= return . fmap demoteHeaders
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+      route $ setExtension "html"
+      compile $
+        pandocCompilerWithOpts
+          >>= loadAndApplyTemplate "templates/post.html" postCtx . fmap demoteHeaders
+          >>= loadAndApplyTemplate "templates/default.html" postCtx
+          >>= relativizeUrls
 
     create ["archive.html"] $ do
-        route $ constRoute "pages/archive.html"
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) <>
-                    constField "title" "archives"            <>
-                    defaultContext
+      route $ constRoute "pages/archive.html"
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let archiveCtx =
+              listField "posts" postCtx (return posts)
+                <> constField "title" "archives"
+                <> defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+          >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+          >>= relativizeUrls
 
     match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) <>
-                    defaultContext
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let indexCtx =
+              listField "posts" postCtx (return posts)
+                <> defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+        getResourceBody
+          >>= applyAsTemplate indexCtx
+          >>= loadAndApplyTemplate "templates/default.html" indexCtx
+          >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
@@ -126,5 +130,5 @@ main = do
 
 postCtx :: Context String
 postCtx =
-    dateField "date" "%Y / %m / %d" <>
-    defaultContext
+  dateField "date" "%Y / %m / %d"
+    <> defaultContext
